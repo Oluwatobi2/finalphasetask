0ba6440aa3091b2657b02567e94755d10098e546
Afolabi Oluwatobi <afolabiseun@gmail.com>
2022-06-17 22:53:34 -0700

    My final phase task

--- controller/indexcontroller.js
+++ controller/indexcontroller.js
@@ -1,5 +1,6 @@
  const { Mongoose } = require('mongoose');
 const { userSchema } = require('../model/indexmodel');
+const axios = require('axios');
  
  //CREATE A NEW LOCATION DATA
  function create (req, res){
@@ -76,33 +77,16 @@ function fetch (req, res){
 
 //CALCULATING DISTANCE BETWEEN LOCATIONS
 function distBtwnLocation(req, res){
-     
-     function calcDist (lat1, lon1, lat2, lon2){
-         if ((lat1 == lat2) && (lon1 == lon2)) {
-             return 0;
-         }
-            var R = 6371; //km
-            var dLat = toRad(lat2 - lat1);
-            var dLon = toRad(lon2 - lon1);
-            var lat1 = toRad(lat1);
-            var lat2 = toRad(lat2);
-
-            var a = Math.sin(dLat/2) * Math.sin(dLat/2) + Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2);
-            var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-            var d = R * c
-            return d;
-     }
-      function toRad(Value){
-          return Value * Math.PI / 180;
-      }
-    
-    alert(calcDist(6.5227,5.4527,3.6218,7.5248).toFixed(1));
-    console.log(Value(toRad()));
+        const distance = req.distance;
+        const latitudeLocation = req.userSchema.Coordinates[0];
+        const longitudeLocation = req.userSchema.Coordinates[1];
 
     res.json({
         successful: true,
         message:"Distance Calculated",
-       
+       data:{
+           distance
+       }
     })
 };
  
